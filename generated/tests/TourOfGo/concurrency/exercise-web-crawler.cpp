8a901e29914dd09b33adcd36581217710ca63ad8
Sébastien Mirabel <seb_mirabel-github@yahoo.fr>
2024-04-17 03:58:32 +0200

    [generated files update]

--- generated/tests/TourOfGo/concurrency/exercise-web-crawler.cpp
+++ generated/tests/TourOfGo/concurrency/exercise-web-crawler.cpp
@@ -112,7 +112,7 @@ namespace golang
         return {"", nullptr, fmt::Errorf("not found: %s", url)};
     }
 
-    auto fetcher = gocpp::Init<fakeFetcher>([](fakeFetcher& x) { x.&{%!s(token.Pos=1169) STRING "https://golang.org/"} = new fakeResult {"The Go Programming Language", gocpp::slice<std::string> {"https://golang.org/pkg/", "https://golang.org/cmd/"}}; x.&{%!s(token.Pos=1320) STRING "https://golang.org/pkg/"} = new fakeResult {"Packages", gocpp::slice<std::string> {"https://golang.org/", "https://golang.org/cmd/", "https://golang.org/pkg/fmt/", "https://golang.org/pkg/os/"}}; x.&{%!s(token.Pos=1519) STRING "https://golang.org/pkg/fmt/"} = new fakeResult {"Package fmt", gocpp::slice<std::string> {"https://golang.org/", "https://golang.org/pkg/"}}; x.&{%!s(token.Pos=1658) STRING "https://golang.org/pkg/os/"} = new fakeResult {"Package os", gocpp::slice<std::string> {"https://golang.org/", "https://golang.org/pkg/"}}; });
+    auto fetcher = gocpp::Init<fakeFetcher>([](fakeFetcher& x) { x."https://golang.org/" = new fakeResult {"The Go Programming Language", gocpp::slice<std::string> {"https://golang.org/pkg/", "https://golang.org/cmd/"}}; x."https://golang.org/pkg/" = new fakeResult {"Packages", gocpp::slice<std::string> {"https://golang.org/", "https://golang.org/cmd/", "https://golang.org/pkg/fmt/", "https://golang.org/pkg/os/"}}; x."https://golang.org/pkg/fmt/" = new fakeResult {"Package fmt", gocpp::slice<std::string> {"https://golang.org/", "https://golang.org/pkg/"}}; x."https://golang.org/pkg/os/" = new fakeResult {"Package os", gocpp::slice<std::string> {"https://golang.org/", "https://golang.org/pkg/"}}; });
 }
 
 int main()
